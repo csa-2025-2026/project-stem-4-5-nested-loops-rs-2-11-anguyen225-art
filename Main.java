2a8cdbaae923eabb88a5c10e2528837df4704b58
anguyen225-art <anguyen225@student.hbuhsd.edu>
2025-10-27 21:45:42 +0000

    classwork211

--- Main.java
+++ Main.java
@@ -4,17 +4,35 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    printNTimes("hello", 5);
+    printNums();
+
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)  // for each letter in the word... (loop)
+    {
+      String letter = word.substring(i, i + 1);
+      for (int j = 0; j < N; j++)                  // print that letter N times (loop)
+      {
+        System.out.print(letter);
+      }
+      System.out.println();
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    for (int a = 10; a > 0; a--)
+    {
+      String output = "";
+      for (int b = a; b > 0; b--)
+      {
+        System.out.print(output);
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
